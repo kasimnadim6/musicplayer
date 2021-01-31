26a3f2b67930bbf3647559cb8638a7ac58c4798d
Mahammad Kasim <kasimnadim6@gmail.com>
2021-01-31 22:01:06 +0530

    initial styles and component creation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './styles/app.scss';
+import React from "react";
+import "./styles/app.scss";
+import Song from "./component/Song";
+import Player from "./component/Player";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Musicplayer</h1>
+    <div className='App'>
+      <Song />
+      <Player />
     </div>
   );
 }
---
+++ src/component/Song.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Song = () => {
+    return (
+        <div className="song-container">
+            <h1>song</h1>
+        </div>
+    )
+}
+
+export default Song;
